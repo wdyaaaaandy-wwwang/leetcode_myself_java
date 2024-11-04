05d16f1c8d83662bc509fdd8210b3cde505e1f31
wangdingyue <2412007086@qq.com>
2024-11-04 18:39:42 +0800

    18：39 更改了项目的结构，其它部分不变

---
+++ src/shoppingOffers.java
@@ -0,0 +1,11 @@
+/**
+ * @author Andy_Wang
+ * @version 1.0
+ * @description: 638.大礼包 中等 回溯
+ * @date 2024/11/4 16:22
+ */
+public class shoppingOffers {
+    public static void main(String[] args) {
+
+    }
+}
